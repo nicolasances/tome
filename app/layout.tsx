417c363f96a3f87091d745cfadeba3b0128a3577
nicolasances <nicolas.matteazzi@gmail.com>
2025-01-09 06:22:55 +0100

    PWA setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,14 +30,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <script src="https://accounts.google.com/gsi/client" async defer></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="theme-color" content="#00acc1" />
+        <meta name="description" content="Tome app" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        {/* <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        /> */}
+        <title>Tome</title>
       </head>
       <body
         className={`${appFont.className} antialiased min-h-screen flex flex-col`}
       >
         <div className="">
-          <Header/>
+          <Header />
         </div>
         <div className="app-content text-base flex flex-1 flex-col justify-start px-6 md:px-[25vw]">
           {children}
