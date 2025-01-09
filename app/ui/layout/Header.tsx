6132a13ce34010a0f68b1ad587a8ab7b0aa2b0fa
nicolasances <nicolas.matteazzi@gmail.com>
2025-01-09 08:32:46 +0100

    PWA - Fixed build errors

--- app/ui/layout/Header.tsx
+++ app/ui/layout/Header.tsx
@@ -1,14 +1,8 @@
 'use client'
 
-import { usePathname, useRouter } from "next/navigation";
 import TomeLogo from "../graphics/TomeLogo";
 
 export default function Header() {
-
-    const pathname = usePathname()
-    const router = useRouter()
-
-    const isRoot = pathname == '/'
 
     return (
         <div className="app-header flex flex-row items-center px-4">
