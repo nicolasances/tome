60b0b8956880bf48cb8cd05ecba7ab0d7d559e05
nicolasances <nicolas.matteazzi@gmail.com>
2025-01-29 21:20:40 +0100

    Memorization Levels Mock Mobile

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,19 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getStoredUserToken, googleSignIn } from "@/utils/AuthUtil";
 import { AuthAPI } from "@/api/AuthAPI";
-import TopicsCard from "./ui/cards/TopicsCard";
+// import TopicsCard from "./ui/cards/TopicsCard";
 import Book from "./ui/graphics/icons/Book";
 import { TomeAPI } from "@/api/TomeAPI";
 import { TopicReview } from "@/model/topicReview";
 import NextSVG from "./ui/graphics/icons/Next";
 import Footer from "./ui/layout/Footer";
+import MemLevel from "./ui/graphics/MemLevel";
 
 export default function Home() {
 
   const [loginNeeded, setLoginNeeded] = useState<boolean | null>(null)
   const [runningTopicReview, setRunningTopicReview] = useState<TopicReview | undefined>(undefined)
+  const [topicMemorizationLevels, setTopicMemorizationLevels] = useState<any>(undefined)
 
   const router = useRouter()
 
@@ -88,25 +90,44 @@ export default function Home() {
 
   }
 
+  /**
+   * Loads for each topic its memorization level
+   */
+  const loadTopicMemorizationLevels = async () => {
+
+    const response = {
+      topics: [
+        { title: 'Rome', memorizationLevel: 69 },
+        { title: 'History of Japan', memorizationLevel: 46 },
+        { title: 'History of China', memorizationLevel: 12 },
+        { title: 'The Vietnam Wars', memorizationLevel: 7 },
+        { title: 'History of Middle Ages', memorizationLevel: 0 },
+      ]
+    }
+
+    setTopicMemorizationLevels(response.topics)
+  }
+
   useEffect(() => { verifyAuthentication() }, [])
   useEffect(() => { triggerSignIn() }, [loginNeeded])
   useEffect(() => { loadRunningTopicReview() }, [loginNeeded])
+  useEffect(() => { loadTopicMemorizationLevels() }, [loginNeeded])
 
   // Empty screen while Google SignIn is loading
   if (loginNeeded == null) return (<div></div>)
   if (loginNeeded === true) return (<div></div>)
 
   return (
-    <div className="flex flex-1 flex-col items-stretch justify-start space-y-4">
+    <div className="flex flex-1 flex-col items-stretch justify-start">
       <div className="flex flex-row space-x-4 items-center">
         {/* <div className="flex-1"><DailyProgress/></div> */}
         <div className="flex-1"><PowerCard perc={45} /></div>
       </div>
-      <div className="">
+      {/* <div className="">
         <TopicsCard />
-      </div>
+      </div> */}
       {runningTopicReview &&
-        <div className="flex flex-row items-center justify-center -mx-2 px-3 space-x-2">
+        <div className="flex flex-row items-center justify-center px-3 space-x-2 mt-4">
           <div className="w-10 h-10 p-2 fill-cyan-200 border border-2 rounded border-cyan-200"><Book /></div>
           <div className="flex flex-col flex-1 md:flex-none">
             <div className="text-sm">You have a running Topic Review</div>
@@ -115,6 +136,21 @@ export default function Home() {
           <div className="md:pl-8">
             <RoundButton icon={<NextSVG />} size="s" onClick={() => { router.push(`/tr/${runningTopicReview?.id}`) }} />
           </div>
+        </div>
+      }
+      {
+        topicMemorizationLevels &&
+        <div className="mx-3 border-cyan-700 mt-8">
+          <div className="text-sm mb-2">Your Memorization Levels</div>
+          {topicMemorizationLevels.map((topic: any, index: number) => {
+            // const isLastElement = index === topicMemorizationLevels.length - 1;
+            return (
+              <div className={`flex flex-row items-center `} key={topic.title} >
+                <div className="mr-2"><MemLevel perc={topic.memorizationLevel} /></div>
+                <div className="flex-1">{topic.title}</div>
+              </div>
+            );
+          })}
         </div>
       }
       <div className="flex-1"></div>
---
+++ app/ui/graphics/MemLevel.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+export default function MemLevel({ perc }: Readonly<{ perc: number }>) {
+
+    const circleRadius = 64; // Radius of the circle
+    const circumference = 2 * Math.PI * circleRadius; // Circumference of the circle
+    const offset = circumference - (perc / 100) * circumference;
+
+    return (
+        <div className="flex flex-row bg-transparent rounded-md items-center justify-center h-10 min-w-10 space-x-1">
+            <div className="relative w-8 h-8 flex items-center justify-center">
+                <svg
+                    className="transform -rotate-90 scale-y-[-1]"
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 160 160"
+                >
+                    {/* Background border */}
+                    <circle
+                        cx="80"
+                        cy="80"
+                        r={circleRadius}
+                        stroke="#0891b2"
+                        strokeWidth="14"
+                        fill="none"
+                    />
+                    {/* Highlighted portion */}
+                    <circle
+                        cx="80"
+                        cy="80"
+                        r={circleRadius}
+                        stroke="#d9f99d"
+                        strokeWidth="12"
+                        fill="none"
+                        strokeDasharray={circumference}
+                        strokeDashoffset={offset}
+                        strokeLinecap="round"
+                    />
+                </svg>
+                <div className="absolute text-base font-400 text-cyan-950 flex justify-center items-center">
+                    <svg className={`fill-cyan-100`} style={{opacity: `${perc + 20}%`}} width="50%" height="50%" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <title>lightning-bolt</title>
+                        <path d="M23.5 13.187h-7.5v-12.187l-7.5 17.813h7.5v12.187l7.5-17.813z"></path>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    )
+}
