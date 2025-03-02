81f438af5de93883619d1a22771e63c4f4d39257
nicolasances <nicolas.matteazzi@gmail.com>
2025-03-02 12:34:53 +0100

    Fixed new topic page layout

--- app/new-topic/page.tsx
+++ app/new-topic/page.tsx
@@ -32,24 +32,25 @@ export default function NewTopic() {
     }
 
     return (
-        <div className="flex flex-1 flex-col items-stretch text-base md:text-lg">
+        <div className="flex flex-1 flex-col items-stretch justify-start">
 
-            <div className="">
-                To upload a new topic, please copy below the URL of the blog you want to upload.
-            </div>
+            <div className="flex-1 app-content">
 
-            <div className="grid w-full items-center gap-1.5 mt-4">
-                <Label htmlFor="email">URL of the Blog</Label>
-                <Input disabled={uploading} className="text-base md:text-lg" type="url" id="email" placeholder="Insert the URL of the Blog" onChange={(v) => { setBlogUrl(v.target.value) }} />
-            </div>
-
-            {uploading &&
-                <div className="mt-4 pl-2">
-                    <LoadingBar label="Uploading the Blog.." />
+                <div className="">
+                    To upload a new topic, please copy below the URL of the blog you want to upload.
                 </div>
-            }
 
-            <div className="flex-1"></div>
+                <div className="grid w-full items-center gap-1.5 mt-4">
+                    <Label htmlFor="email">URL of the Blog</Label>
+                    <Input disabled={uploading} className="text-base md:text-lg" type="url" id="email" placeholder="Insert the URL of the Blog" onChange={(v) => { setBlogUrl(v.target.value) }} />
+                </div>
+
+                {uploading &&
+                    <div className="mt-4 pl-2">
+                        <LoadingBar label="Uploading the Blog.." />
+                    </div>
+                }
+            </div>
 
             <Footer>
                 <div className="flex justify-center items-center space-x-2">
