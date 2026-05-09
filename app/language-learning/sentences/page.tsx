5577cc45d08b1a81f21a71ece5214b5328068c25
nicolasances <nicolas.matteazzi@gmail.com>
2026-05-09 17:13:41 +0200

    UI fixes on Sentence page

--- app/language-learning/sentences/page.tsx
+++ app/language-learning/sentences/page.tsx
@@ -85,7 +85,7 @@ export default function SentencesPage() {
 
                 {/* Sentences list */}
                 {sentences !== null && sentences.length > 0 && (
-                    <div className="flex flex-col gap-3 mt-4 pb-4">
+                    <div className="flex flex-col mt-4">
                         {sentences.map((sentence) => (
                             <SentenceRow key={sentence.id} sentence={sentence} />
                         ))}
@@ -100,7 +100,7 @@ export default function SentencesPage() {
 
 function SentenceRow({ sentence }: { sentence: Sentence }) {
     return (
-        <div className="rounded-2xl border border-cyan-700 p-3">
+        <div className="border-b border-cyan-600 py-2">
             <div className="text-sm font-medium text-grey-900">{sentence.sentence}</div>
             <div className="text-xs text-muted-foreground mt-1">{sentence.translation}</div>
         </div>
