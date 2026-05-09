14e5cc81510dd8ace423a48f4c5e819db0b7d646
nicolasances <nicolas.matteazzi@gmail.com>
2026-05-09 17:51:16 +0200

    Graph positioning

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               <CarModeContextProvider>
                 <HeaderProvider>
                   <TomeHeader />
-                  <div className="flex-1 overflow-y-auto">
+                  <div className="flex flex-1 flex-col min-h-0 overflow-y-auto">
                     <TomeContextProvider>
                       {children}
                     </TomeContextProvider>
