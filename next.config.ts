f063a787650ee4edc82157dcc7d14a4f69434794
nicolasances <nicolas.matteazzi@gmail.com>
2026-05-14 16:35:07 +0200

    Restore to package mode

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['toto-react'],
-  webpack: (config) => {
-    const path = require("path");
-    config.resolve.alias['toto-react$'] = path.resolve(__dirname, '../toto-react/src/index.ts');
-    return config;
-  },
 };
 
 export default nextConfig;
