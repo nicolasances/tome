eee15b8bfebaf8d400d9f10057ed2083486fcd07
nicolasances <nicolas.matteazzi@gmail.com>
2026-06-14 07:44:57 +0200

    Small CSS fixes

--- app/language-learning/module/[moduleId]/practice/components/ExFillBlank.tsx
+++ app/language-learning/module/[moduleId]/practice/components/ExFillBlank.tsx
@@ -45,7 +45,7 @@ export function ExFillBlank({exercise, submissionState, inputValue, onInputChang
                     </div>
                 )}
                 {exercise.promptTranslation && (
-                    <p className="text-sm text-black/50 mt-3 text-center">{exercise.promptTranslation}</p>
+                    <p className="text-base text-black/50 mt-3 text-center">{exercise.promptTranslation}</p>
                 )}
             </div>
 
